89156fff301914f6831d39754ddfd85830413465
SunlightLuck <hope4wd88@gmail.com>
2023-12-21 02:37:27 -0500

    init

---
+++ src/pages/newtab/Newtab.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import "@pages/newtab/Newtab.css"
+
+export default function Newtab(): JSX.Element {
+  return <div className="App">New Tab</div>
+}
---
+++ src/pages/popup/Popup.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Popup(): JSX.Element {
+  return <div className="text-red-400">Here's New Popup</div>
+}
